cec2a7695525f02346524307346c0f2b3a3c0a9a
Chaz Haskins <chaz@socialdriver.com>
2016-02-12 14:38:33 -0500

    removed validatorjs

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -34,6 +34,7 @@ module.exports = function(config) {
       'bower_components/highcharts-ng/dist/highcharts-ng.js',
       'bower_components/countUp.js/countUp.js',
       'bower_components/ng-countUp.js/ngCountUp.min.js',
+      'bower_components/bootstrap-validator/dist/validator.min.js',
       'bower_components/angular-mocks/angular-mocks.js',
       // endbower
       "app/scripts/**/*.js",
